7db5d7608674220b0958ebf4ce3ac11e8a6ef78c
ddecoen <ddecoen@gmail.com>
2023-07-15 20:16:01 -0700

    update files

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ddecoen/command_line_applications
 
 go 1.18
+
+require gonum.org/v1/gonum v0.13.0 // indirect
--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
-	"io"
+	"log"
 	"os"
+	"strconv"
+
+	"gonum.org/v1/gonum/stat"
 )
 
 func main() {
@@ -19,23 +22,44 @@ func main() {
 	// Create a new CSV reader
 	reader := csv.NewReader(file)
 
-	// Read the CSV records one by one
-	for {
-		// Read a record
-		record, err := reader.Read()
+	// Read all the records from the CSV file
+	// Read a record
+	records, err := reader.ReadAll()
 
-		// Stop reading if we reach the end of the file
-		if err == io.EOF {
-			break
-		}
-
-		// Handle other errors
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-
-		// Process the record
-		fmt.Println(record)
+	// Stop reading if we reach the end of the file
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	//Extract values from the records
+	var values []float64
+	for _, record := range records {
+		for _, field := range record {
+			//Convert the field to float64
+			value, err := strconv.ParseFloat(field, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			values = append(values, value)
+		}
+	}
+
+	// Process the record
+	//fmt.Println(record)
+
+	// Calculate summary statistics
+	//min := math.Min(values)
+	//max := math.Max(values)
+	mean := stat.Mean(values, nil)
+	median := stat.Quantile(0.5, stat.Empirical, values, nil)
+	stddev := stat.StdDev(values, nil)
+	variance := stat.Variance(values, nil)
+
+	// Print the summary statistics
+	//fmt.Println("Minimum:", min)
+	//fmt.Println("Maximum:", max)
+	fmt.Println("Mean:", mean)
+	fmt.Println("Median:", median)
+	fmt.Println("Standard Deviation:", stddev)
+	fmt.Println("Variance:", variance)
 }
